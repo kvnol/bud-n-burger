90726276e8c39d631e237222c652f5fd6d69a795
Kevin Oliveira <me@kevinoliveira.com.br>
2017-08-30 23:25:49 -0300

    Create main strucutre

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,31 @@
 const gulp = require('gulp');
 const concat = require('gulp-concat');
 const ts = require('gulp-typescript');
+const clean = require('gulp-clean-css');
 
-gulp.task('default');
+const files = {
+  'js': [ 
+    './assets/js/_parts/*.js'
+  ],
+  'cssVendor': [
+    './node_modules/normalize.css/normalize.css'
+  ]
+}
+
+gulp.task('js', () => {
+  gulp.src(files.js)
+    .pipe('main.js')
+    .pipe(ts())
+    .pipe(gulp.dest('assets/js'))
+});
+
+gulp.task('css', () => {
+  gulp.src(files.cssVendor)
+    .pipe(concat('vendor.css'))
+    .pipe(clean({
+      compatibility: 'ie9'
+    }))
+    .pipe(gulp.dest('assets/css'))
+});
+
+gulp.task('default', ['css']);
